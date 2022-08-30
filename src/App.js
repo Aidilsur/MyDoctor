0353c761f7b65357a9f75b0be93a92936cca4486
unknown <aidilsurya9@gmail.com>
2022-08-30 11:00:20 +0700

    error message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import {NavigationContainer} from '@react-navigation/native';
 import * as React from 'react';
 import Router from './Router';
+import FlashMessage from 'react-native-flash-message';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Router />
-    </NavigationContainer>
+    <>
+      <NavigationContainer>
+        <Router />
+      </NavigationContainer>
+      <FlashMessage position="top" />
+    </>
   );
 }
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -4,6 +4,7 @@ import {Button, Gap, Header, Input, Loading} from '../../components';
 import {colors, useForm} from '../../utils';
 import {getAuth, createUserWithEmailAndPassword} from 'firebase/auth';
 import {Firebase} from '../../config';
+import {showMessage, hideMessage} from 'react-native-flash-message';
 
 const Register = ({navigation}) => {
   const [form, setForm] = useForm({
@@ -28,10 +29,16 @@ const Register = ({navigation}) => {
         // ...
       })
       .catch(error => {
-        const errorMessage = error?.message;
+        const errorMessage = error.message;
         // ..
         setLoading(false);
-        console.log('register gagal: ', errorMessage);
+        showMessage({
+          message: errorMessage,
+          type: 'default',
+          backgroundColor: colors.error,
+          color: colors.white,
+        });
+        console.log('error : ', error);
       });
   };
 
